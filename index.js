cbdc766e61d94ade02e0f911f066deda3f3aba23
Daniel Ferreras Jr <41604970+iamdanielferreras@users.noreply.github.com>
2026-02-11 14:21:48 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+ console.log(username); 
+username = prompt("what is your username:");
+username = Elert("what is your username:");
+document.getElementById("mySubmit").onclick = function(){ }
+document.getElementById("myH1").textContent = `Welcome ${username}`;
+let price = document.getElementById("price").value;
+
+subResult.textContent = `You are Subcribed`;
+mySubmit.onclick = function(){
+price = Number(price);
+
+if (condition) {
+    // This code runs if the condition is TRUE
+} else {
+    // This code runs if the condition is FALSE
+}
+*/
+
+function rollDice(){
+    const numOfDice =  document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for (let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        console.log(values);
+        images.push(`<img src="dice_images/dice-${value}.png" alt="Dice ${value}">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(`, `)}`;
+    diceImages.innerHTML = images.join(``);
+}
